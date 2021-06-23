2baaeac1e7bdbefd8f914178b1f00945f3dab7ec
jensengbg-jack-carling <72305598+jensengbg-jack-carling@users.noreply.github.com>
2021-06-23 16:52:26 +0200

    Update api.js
    • Update CORS block

--- heroku/api.js
+++ heroku/api.js
@@ -1,48 +1,50 @@
 module.exports = (app) => {
-  const cors = require('cors');
-
-  const whiteList = ['https://minesweeper-ts.herokuapp.com/'];
-  const corsOptions = {
-    origin: (origin, callback) => {
-      if (whiteList.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-  };
-
-  app.use(cors(corsOptions));
-
   const Highscore = require('./models');
   app.post('/api/highscore', async (req, res) => {
-    const date = Date.now();
-    const data = { date, ...req.body };
-    const db = new Highscore(data);
-    try {
-      await db.save();
-    } catch (error) {
-      console.log(error);
-    }
-    res.json({ success: true });
-  });
-  app.get('/api/highscore/:difficulty', async (req, res) => {
-    const param = req.params.difficulty;
-
+    const host = req.hostname;
     let response = { success: true };
-    let data;
-    if (param === 'beginner') {
-      data = await Highscore.find({ difficulty: 'Beginner' }).sort({ time: 1 }).limit(25);
-    } else if (param === 'intermediate') {
-      data = await Highscore.find({ difficulty: 'Intermediate' }).sort({ time: 1 }).limit(25);
-    } else if (param === 'expert') {
-      data = await Highscore.find({ difficulty: 'Expert' }).sort({ time: 1 }).limit(25);
-    } else if (param === 'custom') {
-      data = await Highscore.find({ difficulty: 'Custom' }).sort({ percent: -1, time: 1 }).limit(25);
+    if (host === 'minesweeper-ts.herokuapp.com') {
+      const date = Date.now();
+      const data = { date, ...req.body };
+      const db = new Highscore(data);
+      try {
+        await db.save();
+      } catch (error) {
+        console.log(error);
+      }
     } else {
       response = {
         success: false,
-        message: 'Error: Invalid param - beginner / intermediate / expert / custom',
+        message: 'Error: Invalid host',
+      };
+    }
+    res.json(response);
+  });
+  app.get('/api/highscore/:difficulty', async (req, res) => {
+    const host = req.hostname;
+    const param = req.params.difficulty;
+    let response = { success: true };
+    let data;
+
+    if (host === 'minesweeper-ts.herokuapp.com') {
+      if (param === 'beginner') {
+        data = await Highscore.find({ difficulty: 'Beginner' }).sort({ time: 1 }).limit(25);
+      } else if (param === 'intermediate') {
+        data = await Highscore.find({ difficulty: 'Intermediate' }).sort({ time: 1 }).limit(25);
+      } else if (param === 'expert') {
+        data = await Highscore.find({ difficulty: 'Expert' }).sort({ time: 1 }).limit(25);
+      } else if (param === 'custom') {
+        data = await Highscore.find({ difficulty: 'Custom' }).sort({ percent: -1, time: 1 }).limit(25);
+      } else {
+        response = {
+          success: false,
+          message: 'Error: Invalid param - beginner / intermediate / expert / custom',
+        };
+      }
+    } else {
+      response = {
+        success: false,
+        message: 'Error: Invalid host',
       };
     }
     response.data = data;
